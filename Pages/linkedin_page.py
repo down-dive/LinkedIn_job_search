8d49cd2292b2e340931be4b88ab56876d6406df2
down-dive <jter1990@gmail.com>
2024-02-07 09:27:41 -0800

    fixed the .self issue

--- Pages/linkedin_page.py
+++ Pages/linkedin_page.py
@@ -55,7 +55,7 @@ class LinkedInPage:
 
         anytime_button = self.wait.until(EC.element_to_be_clickable(DateFilterLocators.ANYTIME_BUTTON))
         anytime_button.click()
-        time.sleep(5)
+        time.sleep(7)
 
         past_week_button = self.wait.until(EC.element_to_be_clickable(DateFilterLocators.PAST_WEEK_BUTTON))
         past_week_button.click()
@@ -69,39 +69,39 @@ class LinkedInPage:
     def click_job_title(self):
         print("I am on titles")
         # Clicking on each job title on the page
-        # titles = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
-        # Wait for the presence and visibility of all elements
-        titles = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
-        print("Titles are located")
+        # titles =self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
+        #self.wait for the presence and visibility of all elements
+        # titles = self.wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
         
-
-        # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
-
-        # titles = driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
+        self.wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
+        time.sleep(5)
+        titles = self.driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
+        print("Titles are located")
         
         for index in range(len(titles)):
             if index < len(titles):
                 print(len(titles))
                 timeout = 10
                 try:
-                    titles = driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
+                    titles = self.driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
+                    print("I am self")
                     title = titles[index]
                     element_present = EC.presence_of_element_located((By.XPATH, f'(//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"])[{index + 1}]'))
                     WebDriverWait(driver, timeout).until(element_present)
 
                     title.click()
                     print(f"I am clicking on title {index + 1}")
-                    current_url = driver.current_url
+                    current_url = self.driver.current_url
                     print("Current Webpage URL:", current_url)
 
                     expand_button_locator = (By.XPATH, '//*[@aria-label="Show more, visually expands previously read content above"]')
-                    wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
+                    self.wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
                     print("I am expanding the page")
 
-                    job_title_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
-                    company_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
-                    location_extracted = driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
-                    job_description_extracted = driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
+                    job_title_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
+                    company_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
+                    location_extracted = self.driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
+                    job_description_extracted = self.driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
 
                     found_keywords = [keyword for keyword in keywords if keyword.lower() in job_description_extracted.lower()]
 
@@ -118,14 +118,14 @@ class LinkedInPage:
 
                 except (TimeoutException, StaleElementReferenceException) as e:
                     print(f"Exception occurred: {e}. Reloading the page...")
-                    driver.refresh()
+                    self.driver.refresh()
                     expand_button_locator = (By.XPATH, '//*[@aria-label="Show more, visually expands previously read content above"]')
-                    wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
+                    self.wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
                     print("I am expanding the page")
-                    job_title_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
-                    company_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
-                    location_extracted = driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
-                    job_description_extracted = driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
+                    job_title_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
+                    company_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
+                    location_extracted = self.driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
+                    job_description_extracted = self.driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
                     
                     found_keywords = [keyword for keyword in keywords if keyword.lower() in job_description_extracted.lower()]
 
@@ -148,54 +148,54 @@ class LinkedInPage:
     # try:
     #     modified_url = 'https://www.linkedin.com/jobs/sqa-engineer-jobs-cupertino-ca?position=1&pageNum=0'
     #     driver.get(modified_url)
-    #     wait = WebDriverWait(driver, 10)
+    #     self.wait = WebDriverWait(driver, 10)
     #     print("I am loading the page")
 
     #     driver.maximize_window()
     #     print("I am expanding the page")
 
     #     # Change the date filter
-    #     # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@aria-label="Date posted filter. Any Time filter is currently applied. Clicking this button displays all Date posted filter options." ]'))).click()
+    #     #self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@aria-label="Date posted filter. Any Time filter is currently applied. Clicking this button displays all Date posted filter options." ]'))).click()
     #     anytime_button = getattr(DateFilterLocators, "ANYTIME_BUTTON")
-    #     wait.until(EC.element_to_be_clickable(anytime_button)).click()
+    #     self.wait.until(EC.element_to_be_clickable(anytime_button)).click()
     #     print(" I am clicking on anythime button ")
     #     time.sleep(5)
     #     past_week_button = getattr(DateFilterLocators, "PAST_WEEK_BUTTON")     
-    #     wait.until(EC.element_to_be_clickable(past_week_button)).click()
+    #     self.wait.until(EC.element_to_be_clickable(past_week_button)).click()
     #     done_button = getattr(DateFilterLocators, "DONE_BUTTON")
-    #     wait.until(EC.element_to_be_clickable(done_button)).click()
+    #     self.wait.until(EC.element_to_be_clickable(done_button)).click()
     #     print("I am changing the date filter")
     #     sys.exit()
 
     #     # Clicking on each job title on the page
-    #     titles = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
+    #     titles =self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
 
-    #     wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
+    #     self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')))
 
-    #     titles = driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
+    #     titles = self.driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
         
     #     for index in range(len(titles)):
     #         if index < len(titles):
     #             timeout = 10
     #             try:
-    #                 titles = driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
+    #                 titles = self.driver.find_elements(By.XPATH, '//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"]')
     #                 title = titles[index]
     #                 element_present = EC.presence_of_element_located((By.XPATH, f'(//*[@data-tracking-control-name="public_jobs_jserp-result_search-card"])[{index + 1}]'))
     #                 WebDriverWait(driver, timeout).until(element_present)
 
     #                 title.click()
     #                 print(f"I am clicking on title {index + 1}")
-    #                 current_url = driver.current_url
+    #                 current_url = self.driver.current_url
     #                 print("Current Webpage URL:", current_url)
 
     #                 expand_button_locator = (By.XPATH, '//*[@aria-label="Show more, visually expands previously read content above"]')
-    #                 wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
+    #                 self.wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
     #                 print("I am expanding the page")
 
-    #                 job_title_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
-    #                 company_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
-    #                 location_extracted = driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
-    #                 job_description_extracted = driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
+    #                 job_title_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
+    #                 company_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
+    #                 location_extracted = self.driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
+    #                 job_description_extracted = self.driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
 
     #                 found_keywords = [keyword for keyword in keywords if keyword.lower() in job_description_extracted.lower()]
 
@@ -214,12 +214,12 @@ class LinkedInPage:
     #                 print(f"Exception occurred: {e}. Reloading the page...")
     #                 driver.refresh()
     #                 expand_button_locator = (By.XPATH, '//*[@aria-label="Show more, visually expands previously read content above"]')
-    #                 wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
+    #                 self.wait.until(EC.element_to_be_clickable(expand_button_locator)).click()
     #                 print("I am expanding the page")
-    #                 job_title_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
-    #                 company_extracted = driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
-    #                 location_extracted = driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
-    #                 job_description_extracted = driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
+    #                 job_title_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__link']").text
+    #                 company_extracted = self.driver.find_element(By.XPATH, "//a[@class='topcard__org-name-link topcard__flavor--black-link']").text
+    #                 location_extracted = self.driver.find_element(By.XPATH, "//span[@class='topcard__flavor topcard__flavor--bullet']").text
+    #                 job_description_extracted = self.driver.find_element(By.XPATH, "//div[@class='show-more-less-html__markup relative overflow-hidden']").text
                     
     #                 found_keywords = [keyword for keyword in keywords if keyword.lower() in job_description_extracted.lower()]
 
